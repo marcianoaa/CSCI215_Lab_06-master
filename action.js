8c67a55754097680d78723a2534bcf0018d87561
Angelena Marciano <angelena29@gmail.com>
2018-10-30 18:15:57 -0400

    initial commit

---
+++ action.js
@@ -0,0 +1,26 @@
+function checkEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+
+}
+
+function checkAge() {
+    var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
+    
+    if() {
+       
+    } else {
+       
+    }
+}
+
+
+
+document.getElementById('name').onkeyup = function() {
+    var name = document.getElementById('name');
+    var regExp = new RegExp('<');
+
+    if(regExp.test(document.getElementById('name').value)) {
+        name.style.
+    }
+}
