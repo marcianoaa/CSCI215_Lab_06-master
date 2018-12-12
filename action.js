7e2fd0dc50bf793de65d2e950c6118c70b08db27
Angelena Marciano <angelena29@gmail.com>
2018-12-12 13:38:53 -0500

    initial commit

--- action.js
+++ action.js
@@ -1,32 +1,58 @@
+
 function checkEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-
 }
+
 
 function checkAge() {
     var regEx = new RegExp('^(([0-9][1-9])|([1-9][0-9])|[1-9])$');
-    
-    if() {
-       
+
+    var userAge = document.getElementById("age");
+    console.log(userAge.value);
+    console.log(userAge.value.length);
+
+    if(  userAge.value.includes(".") || userAge.value.length > 3) {
+        document.getElementById("age").style.backgroundColor = "red";
+        console.log("changed color to red");
     } else {
-       
+        document.getElementById("age").style.backgroundColor = "";
+        console.log("changed color to white");
     }
 }
 
-function checkName() {
 
+
+function checkName(){
+    var regExp = new RegExp("<");
+
+    var userName = document.getElementById("name");
+    console.log(userName.value);
+
+    if (regExp.test(userName.value)) {
+        document.getElementById("name").style.backgroundColor = "red";
+        console.log("changed color to red");
+    }
+    else{
+        document.getElementById("name").style.backgroundColor = "";
+        console.log("changed color to white");
+    }
 }
 
 function validate() {
+    var email = document.getElementById("email");
+    var isEmailGood =  checkEmail(email.value);
+
+    if (isEmailGood){
+        myAjaxFunction();
+    }
+    else{
+        alert("The subscription could not be processed. Please check the information again.")
+    }
 
 }
 
-document.getElementById('name').onkeyup = function() {
-    var name = document.getElementById('name');
-    var regExp = new RegExp('<');
-
-    if(regExp.test(document.getElementById('name').value)) {
-        name.style.
-    }
+function myAjaxFunction() {
+    alert("The AJAX has been made!");
+    console.clear();
 }
